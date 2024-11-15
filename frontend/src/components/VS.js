d5f66a58ea902215158240fa49818cb24d4f333c
szbmrk <szmark16@gmail.com>
2024-11-15 20:58:25 +0100

    .

---
+++ frontend/src/components/VS.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import '../styles/vs.css';
+
+const VS = () => {
+    return (
+        <div className="tournament-bracket">
+            <h1>1v1 Cup</h1>
+        </div>
+    );
+};
+
+export default VS;
